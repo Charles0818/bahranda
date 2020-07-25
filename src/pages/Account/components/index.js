e37504d39a710bff313e1c12113c1a53de27a7ff
charles0818 <charles081889@gmail.com>
2020-07-25 01:00:12 +0100

    initialized Sort component

--- src/pages/Account/components/index.js
+++ src/pages/Account/components/index.js
@@ -2,5 +2,6 @@ import HorizontalNavbar from './HorizontalNavbar';
 import LeftSideBar from './LeftSidebar';
 import * as History from './History';
 import EmptyDataRender from './EmptyDataRender';
+import useSort from './Sort';
 import { Form, Spinners, HttpStatusNotification } from '../../components';
-export { HorizontalNavbar, LeftSideBar, History, EmptyDataRender, Form, Spinners, HttpStatusNotification };
+export { HorizontalNavbar, LeftSideBar, History, useSort, EmptyDataRender, Form, Spinners, HttpStatusNotification };
