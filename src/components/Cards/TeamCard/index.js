51e809969d16ab27a570a60d3d7fcc3aa7596bea
charles0818 <charles081889@gmail.com>
2020-08-08 20:28:32 +0100

    connected teams data to UI

--- src/components/Cards/TeamCard/index.js
+++ src/components/Cards/TeamCard/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import customer from '../../../assets/customer.png';
-const TeamCard = () => {
+const TeamCard = ({ member }) => {
+  const { member_image, member_name, member_role } = member;
   return (
-    <div className="team-card d-flex column border-r-10 margin-right-sm margin-bottom-sm card">
-      <img src={customer} alt="Team member" className="border-top-r-inherit" style={{flex: .6}} />
+    <div className="team-card d-flex column border-r-10 margin-right-sm margin-bottom-sm overflow-h card">
+      <div className="avatar">
+        <img src={member_image} alt="Team member" className="border-top-r-inherit" style={{flex: .6}} />
+      </div>
       <div className=" d-flex column align-items-center bg-white padding-md" style={{flex: .4, width: '100%'}} >
-        <h2 className="font-md">Toyin Tomato</h2>
-        <span className="font-m">Director</span>
+        <h2 className="font-md">{member_name}</h2>
+        <span className="font-m">{member_role}</span>
       </div>
     </div>
   )
