3c5839660e5bb9036ddb95a772e695df5aa3435f
charles0818 <charles081889@gmail.com>
2020-08-08 21:07:36 +0100

    updated slider content

--- src/pages/Home/Components/HeaderSlides/index.js
+++ src/pages/Home/Components/HeaderSlides/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import banner1 from '../../assets/banner_hd.jpg'
-import banner2 from '../../assets/banner_7_edit.jpg'
 import banner3 from '../../assets/banner_12.jpeg'
 import banner4 from '../../assets/banner_4.jpg'
 import { Carousels, Animation } from '../../../components';
@@ -10,8 +9,7 @@ const { Carousel } = Carousels;
 const HeaderSlides = () => {
   const slides = [
     <Slide1 />,
-    <Slide banner={banner2} text="Click = Select = Trade" />,
-    <Slide banner={banner3} text="" />,
+    <Slide banner={banner3} text="Click = Select = Trade" />,
     <Slide banner={banner4} text="Re-writing Africa's Trade Advantage" />,
   ]
   return (
@@ -27,7 +25,7 @@ const Slide1 = () => {
     <div className="header-slide position-relative">
       <img src={banner1} alt="slide" />
       <div className="d-flex column justify-content-center slide-content position-absolute-overlay">
-        <div className="">
+        <div className="padding-horizontal-sm">
           <p className="font-lg color-white font-weight-300 margin-bottom-sm">Bridging the gap between</p>
           <h2 className="color-white font-weight-500 margin-bottom-sm">The Agricultural and Manufacturing Sector</h2>
           <Stagger duration={.5} repeat={true}>
@@ -46,7 +44,7 @@ const Slide = ({ banner, text }) => {
     <div className="header-slide position-relative">
       <img src={banner} alt="slide" />
       <div className="d-flex column justify-content-center slide-content position-absolute-overlay">
-        <div className="">
+        <div className="padding-horizontal-sm">
           <h2 className="color-white font-weight-500 margin-bottom-sm">{text}</h2>
           <FadeInLeft duration={.5} repeat={false}>
             <button onClick={() => push('/auth/register')} className="ripple cursor-pointer padding-md bg-color1 border-r-10 color-white font-sm font-weight-600">Get started</button>
