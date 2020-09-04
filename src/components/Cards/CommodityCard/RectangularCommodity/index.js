2d4856822fc4a9b9e173238de5c814250ec49402
charles0818 <charles081889@gmail.com>
2020-09-04 20:55:10 +0100

    re-design commodity card

--- src/components/Cards/CommodityCard/RectangularCommodity/index.js
+++ src/components/Cards/CommodityCard/RectangularCommodity/index.js
@@ -6,14 +6,14 @@ const { formatting: { formatCurrency } } = utils;
 const CommodityCard = memo(({ commodity }) => {
   const { image, commodity_name, price, id, profit_percentage } = commodity;
   return (
-    <div className="product-card margin-bottom-md margin-right-md">
+    <div className="product-card margin-bottom-md margin-right-md cursor-pointer">
       <div className="thumbnail position-relative margin-bottom-sm">
         <Link to={`/commodities/${id}`}><img src={image} alt="product thumbnail" /></Link>
         <span className={`font-sm bg-yellow in-stock color-white font-weight-600 padding-xsm uppercase position-absolute`}>
           { 'SALE'}
         </span>
       </div>
-      <div className="d-flex column details overflow-h">
+      <div className="d-flex column details padding-horizontal-sm overflow-h">
         <h3 className="product-title font-md capitalize">{commodity_name}</h3>
         <span className="font-sm font-weight-500 color-gray categories">{profit_percentage} return</span>
         <h4 className="price font-md color1 font-weight-600">{formatCurrency(price)}</h4>
