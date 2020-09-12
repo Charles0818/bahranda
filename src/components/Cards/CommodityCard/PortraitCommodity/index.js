b1d4df19a77f1a3cb132364f415c1c771e3e354e
charles0818 <charles081889@gmail.com>
2020-09-12 05:50:58 -0700

    re-design portrait commodity card

--- src/components/Cards/CommodityCard/PortraitCommodity/index.js
+++ src/components/Cards/CommodityCard/PortraitCommodity/index.js
@@ -4,16 +4,16 @@ import { MdKeyboardArrowDown } from 'react-icons/md';
 const PortraitCommodityCard = ({ commodity }) => {
   const { image, commodity_name, id } = commodity;
   return (
-    <div className="portrait-commodity-card padding-horizontal-md padding-vertical-md bg-white margin-right-sm margin-bottom-sm">
-      <div className="thumbnail margin-bottom-md">
-        <img src={image} alt="Commodity thumbnail" />
-      </div>
-      <div className="d-flex justify-content-s-between align-items-center">
-        <h3 className="color1 font-lg">{commodity_name}</h3>
-        <Link to={`/commodities/${id}`}>
+    <div className="portrait-commodity-card padding-horizontal-sm padding-vertical-sm bg-white margin-right-sm margin-bottom-sm">
+      <Link to={`/commodities/${id}`}>
+        <div className="thumbnail margin-bottom-sm">
+          <img src={image} alt="Commodity thumbnail" />
+        </div>
+        <div className="d-flex justify-content-s-between align-items-center">
+          <h3 className="color1 font-md">{commodity_name}</h3>
           <MdKeyboardArrowDown className="font-lg color1" />
-        </Link>
-      </div>
+        </div>
+      </Link>
     </div>
   )
 }
